991385c96b9d28a7751d439ff667a41c0cf07b54
shikhar-noone <shikharchaudhary57@gmail.com>
2023-05-15 00:29:07 +0530

    gpt

--- openai_plus/chat_gpt/language_translator.py
+++ openai_plus/chat_gpt/language_translator.py
@@ -1,10 +1,18 @@
 import openai
-openai.api_key = "sk-2R7ZTpdQwsgbiHrOvIsIT3BIbkFJ3Fd3hel91UzkDw07omI"
+openai.api_key = "sk-7y1tjYh8gcf2ubaJXWXoT3BIbkFJkzPuufJn8FJDIpfSj7pW"
 
 def translate_query(query, language):
 
-    prompt = f"query = '{query}'./n Please translate the query into {language} and return statement only."
-    completions = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=2048, n=1,stop=None,temperature=0.5)
+    prompt = f"query = '{query}'.Please translate the query into {language} and return statement only."
+    print(prompt)
+    completions = openai.Completion.create(
+        engine="text-davinci-002", 
+        prompt=prompt, 
+        max_tokens=2048, 
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
     print(completions)
     message = completions.choices[0].text
     #print(completions)
